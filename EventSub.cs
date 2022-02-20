2c535a5498323d56bef7a05161422b5c07124975
2vitreous <observer.in.a.box@gmail.com>
2022-02-20 10:56:11 +0100

    initial stuff dankCrayon

---
+++ EventSub.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchLib.Api;
+
+namespace Core
+{
+    public class EventSub
+    {
+        public static TwitchAPI TwitchAPI = new();
+
+        public EventSub()
+        {
+            TwitchAPI.Settings.AccessToken = Bot.Token;
+            TwitchAPI.Settings.ClientId = Bot.ClientID;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TwitchLib.Client;
+using TwitchLib.Client.Models;
+using TwitchLib.Communication.Clients;
+using TwitchLib.Communication.Models;
+
+namespace Core
+{
+    public class Core
+    {
+        public static DateTime StartupTime = DateTime.Now;
+
+        static void Main(string[] args)
+        {
+            Bot bot = new();
+            Console.ReadLine();
+        }
+
+    }
+
+    public class Bot
+    {
+        public static readonly string Username = "TWITCH_USERNAME";
+        public static readonly string Token = "ACCESS_TOKEN";
+        public static readonly string ClientID = "CLIENT_ID";
+        public static readonly string InitialChannel = "START_CHANNEL";
+
+        public static TwitchClient Client = new();
+
+        public Bot()
+        {
+            ConnectionCredentials credentials = new ConnectionCredentials(Username, Token);
+            var clientOptions = new ClientOptions
+            {
+                MessagesAllowedInPeriod = 750,
+                ThrottlingPeriod = TimeSpan.FromSeconds(30)
+            };
+            WebSocketClient customClient = new WebSocketClient(clientOptions);
+            Client = new TwitchClient(customClient);
+            Client.Initialize(credentials, InitialChannel);
+
+            Client.OnJoinedChannel += (s, e) =>
+            {
+                Console.WriteLine($"Connected to {e.Channel}");
+            };
+            Client.OnMessageReceived += (s, e) =>
+            {
+            };
+            Client.OnConnected += (s, e) =>
+            {
+                Console.WriteLine("connected");
+            };
+
+            Client.Connect();
+        }
+    }
+}
