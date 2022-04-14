4cc182b3bf0f33a43538fe6c680304188cc87360
2vitreous <observer.in.a.box@gmail.com>
2022-04-14 09:39:05 +0200

    initial work on console stuff

--- Program.cs
+++ Program.cs
@@ -2,53 +2,102 @@
 using TwitchLib.Client.Models;
 using TwitchLib.Communication.Clients;
 using TwitchLib.Communication.Models;
+using Newtonsoft.Json;
 
 namespace Core
 {
-    public class Core
+    public static class Core
     {
-        public static DateTime StartupTime = DateTime.Now;
+        public static Config Config { get; private set; } = default!;
 
-        static void Main(string[] args)
+        private static string[] Commands { get; } = { "connect", "disconnect", "edit", "add", "remove", "help"};
+        static async Task Main(string[] args)
         {
-            Bot bot = new();
-            Console.ReadLine();
+            StreamReader reader = new StreamReader("config.json");
+            string json = reader.ReadToEnd();
+            Config = JsonConvert.DeserializeObject<Config>(json)!;
+
+            reader.Close();
+            reader.Dispose();
+
+            while (true)
+            {
+                string? input = Console.ReadLine();
+                if (input is not null && !Commands.Contains(input!.Split(' ')[0]))
+                {
+                    string command = input!.Split(' ')[0];
+                    switch (command)
+                    {
+                        case "connect":
+                            break;
+                        case "disconnect":
+                            break;
+                        case "edit":
+                            break;
+                        case "add":
+                            break;
+                        case "remove":
+                            break;
+
+                        default:
+                            Console.WriteLine("Unrecognized input!");
+                            PrintHelp();
+                            break;
+                    }
+                }
+                if (input is not null && input.StartsWith("exit"))
+                {
+                    break;
+                }
+            }
         }
 
+        private static void PrintHelp()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"Commands: \n\n");
+            Console.WriteLine("------------------------------------------------------");
+            Console.ForegroundColor= ConsoleColor.Blue;
+
+            Console.WriteLine($"edit <channel> <unsub/sub> <live/title/game/offline>");
+            Console.WriteLine($"edit <channel> update <live/title/game/offline> <new message>\n");
+
+            Console.WriteLine($"add <channel>\n");
+
+            Console.WriteLine($"remove <channel>\n");
+
+            Console.WriteLine($"disconnect");
+            Console.WriteLine($"connect \\\\The program automatically connects, only use this when you disconnected manually\n");
+
+            Console.WriteLine($"exit");
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("------------------------------------------------------");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
     }
 
-    public class Bot
+    
+
+    public class Config
     {
-        public static readonly string Username = "TWITCH_USERNAME";
-        public static readonly string Token = "ACCESS_TOKEN";
-        public static readonly string ClientID = "CLIENT_ID";
-        public static readonly string InitialChannel = "START_CHANNEL";
+        public string? access_token { get; set; }
+        public string? client_id { get; set; }
+        public Channel[]? channels { get; set; }
+    }
 
-        public static TwitchClient Client = new();
-
-        public Bot()
-        {
-            ConnectionCredentials credentials = new ConnectionCredentials(Username, Token);
-            var clientOptions = new ClientOptions
-            {
-                MessagesAllowedInPeriod = 750,
-                ThrottlingPeriod = TimeSpan.FromSeconds(30)
-            };
-            WebSocketClient customClient = new WebSocketClient(clientOptions);
-            Client = new TwitchClient(customClient);
-            Client.Initialize(credentials, InitialChannel);
-
-            Client.OnJoinedChannel += (s, e) =>
-            {
-                Console.WriteLine($"Connected to {e.Channel}");
-            };
-            Client.OnConnected += (s, e) =>
-            {
-                Console.WriteLine("connected");
-                EventSub eSub = new();
-            };
-
-            Client.Connect();
-        }
+    public class Channel
+    {
+        public string? name { get; set; }
+        public string? avatar_url { get; set; }
+        public string? webhook_link { get; set; }
+        public bool live_sub { get; set; }
+        public bool title_sub { get; set; }
+        public bool game_sub { get; set; }
+        public bool offline_sub { get; set; }
+        public string? live_message { get; set; }
+        public string? title_message { get; set; }
+        public string? game_message { get; set; }
+        public string? offline_message { get; set; }
     }
 }
